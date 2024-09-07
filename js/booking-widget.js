5af60a5091bb77dc15c874b739d974029389df52
mrBidone <ab.bz@ukr.net>
2024-09-07 17:31:33 +0200

    FixedAll

--- js/booking-widget.js
+++ js/booking-widget.js
@@ -1,40 +1,29 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const recordBtns = document.querySelectorAll(".recordBtn");
-//   const hiddenLink = document.getElementById("hiddenLink");
-//   if (hiddenLink) {
-//     recordBtns.forEach(function (btn) {
-//       btn.addEventListener("click", function () {
-//         hiddenLink.click();
-//       });
-//     });
-//   } else {
-//     console.error("Скрытая ссылка не найдена");
-//   }
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
-  const originalOpenModal = window.dikidi.openModal;
-
-  // Перехватываем оригинальный метод
-  window.dikidi.openModal = function (a, r) {
-    if (!r) {
-      r = {};
-    }
-
-    // Принудительно устанавливаем поведение открытия модального окна
-    r.start = "1"; // "1" означает открытие модального окна
-
-    // Вызываем оригинальный метод с модифицированными параметрами
-    originalOpenModal(a, r);
-  };
-
-  // Ваша логика для кнопок записи
   const recordBtns = document.querySelectorAll(".recordBtn");
   const hiddenLink = document.getElementById("hiddenLink");
+
   if (hiddenLink) {
     recordBtns.forEach(function (btn) {
       btn.addEventListener("click", function () {
         hiddenLink.click();
+        const observer = new MutationObserver(function (
+          mutationsList,
+          observer
+        ) {
+          mutationsList.forEach(function (mutation) {
+            const modalWindow = document.querySelector('[class*="modal"]');
+            if (modalWindow) {
+              modalWindow.style.display = "flex";
+              modalWindow.style.justifyContent = "center";
+              modalWindow.style.alignItems = "center";
+              observer.disconnect();
+            }
+          });
+        });
+        observer.observe(document.body, {
+          childList: true,
+          subtree: true,
+        });
       });
     });
   } else {
